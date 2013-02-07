b71309d0f038a52c18f83e867ba8b62b36716297
ElderMael <sftwr.mael@gmail.com>
2013-02-07 17:45:50 -0600

    appendTitleImage now supports the three different types of images.

--- fourChan.js
+++ fourChan.js
@@ -1,21 +1,35 @@
 (function ($) {
+
+    function buildProtocolString(secure) {
+        protocol = 'http';
+        if (secure) {
+            protocol += 's';
+        }
+
+        return protocol;
+    }
+
+    function buildImageUrl(protocol, type, number) {
+        return imageUrl = protocol + '://static.4chan.org/image/title/' + number + '.' + type;
+
+    }
+
     $.fn.appendTitleImage = function (options) {
 
         var settings = $.extend({
-            'animated':false,
-            'number':0
+            'type':'jpg',
+            'number':0,
+            'secure':false
         }, options);
 
-        var image = '<img src="https://static.4chan.org/image/title/' + settings.number;
-        if (settings.animated) {
-            image += '.gif">';
-        } else {
-            image += '.jpg">';
-        }
+        var protocol = buildProtocolString(settings.secure);
+
+        var image = '<img src="' + buildImageUrl(protocol, settings.type, settings.number) + '" />';
 
         this.append(image);
 
         return this;
 
     };
+
 })(jQuery);
